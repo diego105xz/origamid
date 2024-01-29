2e8fb65109d0ac538ce09eb8e9cf27eb4461ea21
Diego Melo <diego-melo@uni9.edu.br>
2024-01-28 22:43:22 -0300

    0202Ex

---
+++ JavaScript/0202-Tipos_de_dadosEx/script.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+var carro = 'Corolla';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2023';
+
+
+// Declare uma variável com a sua idade
+var idade = 31;
+
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Diego';
+var sobrenome = 'Melo';
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nome);
+
+console.log(carro, ano, idade, nomeCompleto, frase);
