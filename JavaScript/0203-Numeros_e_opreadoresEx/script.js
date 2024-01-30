65b5ceb18b8256ef7c323493826c366eec76011b
Diego Melo <diego-melo@uni9.edu.br>
2024-01-30 12:01:34 -0300

    0203

---
+++ JavaScript/0203-Numeros_e_opreadoresEx/script.js
@@ -0,0 +1,30 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+//35
+
+// Crie duas expressões que retornem NaN
+var nome = 'Diego';
+var idade = 31;
+var nomeIdade = nome * idade
+
+console.log(nomeIdade);
+
+// Somar a string '200' com o número 50 e retornar 250
+var a = '200';
+var b = 50;
+var ab = +a + b;
+
+console.log(ab);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+
+console.log(peso);
