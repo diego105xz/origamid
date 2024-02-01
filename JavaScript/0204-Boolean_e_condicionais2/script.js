37b48c28e7e21e569662c75c756801cc774a5f20
Diego Melo <diego-melo@uni9.edu.br>
2024-02-01 15:36:00 -0300

    0204

---
+++ JavaScript/0204-Boolean_e_condicionais2/script.js
@@ -0,0 +1,48 @@
+//OPERADORES LÓGICOS &&
+//&& Compara se uma expressão e a outra é verdadeira
+true && true; // true
+true && false; // false
+false && true; // false
+'Gato' && 'Cão'; // 'Cão'
+(5 - 5) && (5 + 5); // 0
+'Gato' && false; // false
+(5 >= 5) && (3 < 6); // true
+
+//Se ambos os valores forem true ele irá retornar o último valor verificado Se algum valor for false ele irá retornar o mesmo e não irá continuar a verificar os próximos
+
+
+//-----------------------------------------------------------
+//OPERADORES LÓGICOS ||
+//|| Compara se uma expressão ou outra é verdadeira
+true || true; // true
+true || false; // true
+false || true; // true
+'Gato' || 'Cão'; // 'Gato'
+(5 - 5) || (5 + 5); // 10
+'Gato' || false; // Gato
+(5 >= 5) || (3 < 6); // true
+
+//Retorna o primeiro valor true que encontrar
+
+//--------------------------------------------------
+//SWITCH
+//Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra chave break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+  case 'Azul':
+    console.log('Olhe para o céu.');
+    break;
+  case 'Vermelho':
+    console.log('Olhe para rosas.');
+    break;
+  case 'Amarelo':
+    console.log('Olhe para o sol.');
+    break;
+  default:
+    console.log('Feche os olhos');
+}
+
+
+
+
