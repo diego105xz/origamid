eb44afc4e15137b3125a69ded6ee9456d7ef8d55
Diego Melo <diego-melo@uni9.edu.br>
2024-02-08 20:06:45 -0300

    0301

---
+++ JavaScript/0301-O_que_e_o_DomEx/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+var url_atual = window.location.href;
+console.log(url_atual);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const classeAtivo = document.querySelector('.ativo');
+console.log(classeAtivo);
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela 
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
